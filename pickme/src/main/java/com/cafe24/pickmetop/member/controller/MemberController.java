088f5021ddbe38590f8de8eccc9d8ea77f0b47c8
pc202-23 <pc202-23@pc202-23-PC>
2016-11-22 17:27:38 +0900

    [김복근]로그인후 메인으로 이동

--- pickme/src/main/java/com/cafe24/pickmetop/member/controller/MemberController.java
+++ pickme/src/main/java/com/cafe24/pickmetop/member/controller/MemberController.java
@@ -66,7 +66,7 @@ public class MemberController {
 			session.setAttribute("generalNick", memberGeneralLogin.get("generalNick"));
 			session.setAttribute("generalLevel", memberGeneralLogin.get("generalLevel"));
 		}
-		return "/index";
+		return "/maintest";
 
 	}
 
